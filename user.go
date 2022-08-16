12abad24ebe8f8242afd45af32900a6877b28db8
PramudyaS <pramudyasaputra89@gmail.com>
2022-08-16 11:06:35 +0700

    add struct

--- user.go
+++ user.go
@@ -1,5 +1,24 @@
 package go_cms
 
-func Users() string {
-	return "Users"
+import "fmt"
+
+type User struct {
+	Name    string
+	Age     int
+	Gender  string
+	Address string
+}
+
+func Users(user User) User {
+	return user
+}
+
+func main() {
+	var user User
+	user.Name = "Pramudya"
+	user.Age = 16
+	user.Gender = "Male"
+	user.Address = "caringin"
+
+	fmt.Println(user)
 }
